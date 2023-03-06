2438bc08fae0ff479b253b782330b1a91eac4437
Luong Quoc Bao <lqbao060899@gmail.com>
2023-03-06 21:08:31 +0700

    Update environment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a703ac85d02d84e9e97f4409e0880881-930940419.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
