3622ac6e1524edfca667bafe85e55cdcf0f6f2c6
Luong Quoc Bao <lqbao060899@gmail.com>
2023-03-10 19:08:46 +0700

    Update .circleci/config.yml

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'a703ac85d02d84e9e97f4409e0880881-930940419.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
